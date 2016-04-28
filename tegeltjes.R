5e46a502cb0b82741ddf612286bfdac8fb235288
EdwinTh <edwinthoen@hotmail.com>
2016-04-28 15:05:36 +0200

    completely rebuild the function

--- tegeltjes.R
+++ tegeltjes.R
@@ -1,72 +1,97 @@
 library(ggplot2)
+library(dplyr)
+library(magrittr)
+library(data.table)
 
 # functie om een ontwerp te maken van de tegeltjes
 # produceert een random patroon
 
 tegels <- function(
-  kleuren,   # vector van kleuren
-  p_vec,     # vector probabilites van de kleuren, gelijke lengte als kleuren en som = 1
+  kleuren  = c('darkblue', 'cyan3', 'lightblue1', 'white'),   # vector van kleuren
+  aantal   = rep(40, 4),     # aantal tegels er kleur
   aantal_h = 8, # aantal tegels in de hoogte
-  aantal_b = 50, # aantal tegels in de breedte
-  max_naast_elkaar = 3
-  ) {
+  aantal_b = 20, # aantal tegels in de breedte
+  max_naast_elkaar = 2) {
   
-  if(length(kleuren) != length(p_vec)){
-    stop('Lengte van kleuren en p_vec niet gelijk')
+  if(length(kleuren) != length(aantal)){
+    stop('Lengte van kleuren en lengte van aantal niet gelijk')
   }
-  if(sum(p_vec) != 1){
-    stop('Som van p_vec moet precies 1 zijn')
+  if(sum(aantal) != aantal_h * aantal_b){
+    stop('Som van aantal moet gelijk zijn aan aantal_h * aantal_b')
   }
   
-  plot_data <- expand.grid(1:aantal_h, 1:aantal_b)
+  plot_data <- expand.grid(1:aantal_h, 1:aantal_b) %>% as.data.table
   colnames(plot_data) <- c('Hoogte', 'Breedte')
-  plot_data$Kleur <- character(nrow(plot_data))
-  ltr <- LETTERS[1:4]
+  plot_data$Kleur <- integer(nrow(plot_data))
   
-  laatste_hoogte <- character(max_naast_elkaar)
-  kleur_vermijden <- NULL
-
-  for(i in 1:nrow(plot_data)){
-    if(is.null(kleur_vermijden)) {
-      plot_data$Kleur[i] <- sample(ltr, 1, ,p_vec)
-    } else {
-      plot_data$Kleur[i] <- sample(ltr[!(ltr %in% kleur_vermijden)] , 1, ,
-                                   p_vec[!(ltr %in% kleur_vermijden)])
-    }
-      
-    if(i %% 8 != 0){
-      laatste_hoogte <- c(plot_data$Kleur[i], laatste_hoogte[1:2])
-    } else {
-      laatste_hoogte <- character(max_naast_elkaar)
-    }
-    laatste_breedte_ind <- c( (i+1) - c(aantal_h,
-                                          aantal_h * 2,
-                                          aantal_h * 3))
-    if( all(laatste_breedte_ind > 1) ){
-      laatste_breedte <- plot_data$Kleur[laatste_breedte_ind]
-    } else {
-      laatste_breedte <- LETTERS[1:3]
-    }
-      
-    if(length(unique(laatste_hoogte)) == 1) {
-       kleur_vermijden <- laatste_hoogte[1]
-      }
-    if(length(unique(laatste_breedte)) == 1) {
-      kleur_vermijden <- c(kleur_vermijden, laatste_breedte[1])
-    }
-    if(length(unique(laatste_hoogte)) != 1 & 
-         length(unique(laatste_breedte)) != 1){
-      kleur_vermijden <- NULL
+  kleuren_to_sample <- rep(1:length(p_vec), p_vec * aantal_h * aantal_b)
+  
+  # functie die een kleur samplet uit de resterende opties
+  # geeft de kleur terug en de kleuren_to_sample met de kleur eruit verwijderd
+  sample_kleur <- function(uitsluiten = 0, 
+                           kts = kleuren_to_sample){
+    if(kts  %>% is_in(uitsluiten) %>% all){
+      stop('Geen geldige oplossing gevonden, start opnieuw')
     }
     
+    geldige_kleur <- FALSE
+    while(geldige_kleur == FALSE){
+      kleur <- sample(kts, 1)
+      if(kleur %>% is_in(uitsluiten) %>% not) {
+        geldige_kleur <- TRUE
+        return_list <- list(kleur = kleur,
+                            kts = 
+                              kts[-((kts == kleur) %>% which.max)])
+      }
+    }
+    return(return_list)
   }
-
+  
+  # functie voor de te selecteren kleur kijkt of er een kleur moet
+  # worden uitgesloten
+  check_kleuren <- function(cur_breedte,
+                            cur_hoogte){
+    if(cur_hoogte > max_naast_elkaar){
+      kleuren_hoogte <- 
+        plot_data[Hoogte %in% (cur_hoogte-(max_naast_elkaar)):(cur_hoogte-1) & 
+                    Breedte == cur_breedte, Kleur] %>% unique
+    } else {
+      kleuren_hoogte <- NULL
+    }
+    
+    if(cur_breedte > max_naast_elkaar){
+      kleuren_breedte <- 
+        plot_data[Breedte %in% (cur_breedte-(max_naast_elkaar)):(cur_breedte-1) & 
+                    Hoogte == cur_hoogte, Kleur] %>% unique
+    } else {
+      kleuren_breedte <- NULL
+    }
+    
+    if(length(kleuren_hoogte) > 1) kleuren_hoogte <- NULL
+    if(length(kleuren_breedte) > 1) kleuren_breedte <- NULL
+    uitsluiten <- c(kleuren_hoogte, kleuren_breedte)
+    if(length(uitsluiten) == 0) uitsluiten <- 0
+    
+    return(uitsluiten)
+  }
+  
+  for(i in 1:(aantal_b-1)){
+    for(j in 1:aantal_h){
+      uitsluiten_iter <- check_kleuren(i, j)
+      kleur_iter      <- sample_kleur(uitsluiten_iter, kleuren_to_sample)
+      plot_data[Hoogte == j & Breedte == i, Kleur := kleur_iter$kleur]
+      kleuren_to_sample <- kleur_iter$kts
+    }
+  }
+  
+  plot_data[ ,Kleur := Kleur %>% as.character]
+  
   ggplot(plot_data, aes(Breedte, Hoogte)) +
     geom_tile(aes(fill = Kleur), col = 'white') +
-    scale_fill_manual(values = c("A" = kleuren[1],
-                                 "B" = kleuren[2],
-                                 "C" = kleuren[3],
-                                 "D" = kleuren[4])) + 
+    scale_fill_manual(values = c('1' = kleuren[1],
+                                 '2' = kleuren[2],
+                                 '3' = kleuren[3],
+                                 '4' = kleuren[4])) + 
     xlab('') + 
     ylab('') + 
     guides(fill = FALSE) + 
@@ -82,15 +107,11 @@ tegels <- function(
       ,axis.text.y = element_blank()
     ) +
     coord_fixed()
+  
 }
 
-beneden <- c('darkblue', 'cyan3', 'lightblue1', 'lightyellow')
-boven <- c('red', 'gold', 'tan1', 'white')
-boven_alt <- c('black', 'grey', 'red', 'white')
-
-tegels(beneden, c(.3,.15,.4,.15), max_naast_elkaar = 3)
-tegels(boven, c(.3,.2,.3,.2))
-tegels(boven_alt, c(.3,.2,.3,.2))
-
-boven_alt2 <- c('darkblue', 'tan4', 'beige', 'burlywood3')
-tegels(boven_alt2, c(.3,.2,.3,.2))
+tegels(kleuren = paste('deepskyblue', 1:3, sep = ''),
+       aantal  = c(59, 59, 29),
+       aantal_h =  7,
+       aantal_b =  21,
+       max_naast_elkaar = 2)
